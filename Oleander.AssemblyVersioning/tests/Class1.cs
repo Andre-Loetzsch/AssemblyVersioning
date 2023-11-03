34b439419ff515d98585d348664c86d7e38ddf7d
Andre-Loetzsch <andre.loetzsch@live.de>
2023-11-03 15:31:29 +0100

    commit v1.2.0.0

--- Oleander.AssemblyVersioning/tests/Class1.cs
+++ Oleander.AssemblyVersioning/tests/Class1.cs
@@ -1,12 +1,23 @@
 ﻿namespace Oleander.AssemblyVersioning.Test
 {
-    public class Class1
+    public class Class1 //: ITest
     {
         public int Test { get; set; }
 
         public void DoSomeThing()
         {
 
+        }
+
+        public int Count { get; }
+        public string GetName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetName(string s)
+        {
+            throw new NotImplementedException();
         }
     }
 
