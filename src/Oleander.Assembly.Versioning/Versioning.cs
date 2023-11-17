3e8cd942e707f26d9395aa30c61663ca95702260
Andre-Loetzsch <andre.loetzsch@live.de>
2023-11-17 15:59:53 +0100

    project cleaned up

--- src/Oleander.Assembly.Versioning/Versioning.cs
+++ src/Oleander.Assembly.Versioning/Versioning.cs
@@ -1,5 +1,4 @@
 ﻿using System.Diagnostics.CodeAnalysis;
-using System.Reflection;
 using Oleander.Assembly.Comparator;
 using Oleander.Assembly.Versioning.ExternalProcesses;
 using SysAssembly = System.Reflection.Assembly;
@@ -280,8 +279,6 @@ public class Versioning
         return new Version(major, minor, build, revision);
     }
 
-
-
     private VersioningResult PrivateUpdateAssemblyVersion()
     {
         var updateResult = new VersioningResult();
@@ -328,46 +325,6 @@ public class Versioning
         return updateResult;
     }
 
-
-    //private bool TryGetProjectRefVersion([MaybeNullWhen(false)] out SysAssembly refAssembly, [MaybeNullWhen(false)] out Version lastCalculatedVersion)
-    //{
-    //    refAssembly = null;
-    //    lastCalculatedVersion = null;
-
-    //    var projectRefAssemblyPath = Path.Combine(this._projectDirName, projectVersionInfoFileName);
-    //    if (!File.Exists(projectRefAssemblyPath)) return false;
-
-    //    var fileContent = File.ReadAllLines(projectRefAssemblyPath).ToList();
-
-    //    if (fileContent.Count < 2) return false;
-
-    //    if (!Version.TryParse(fileContent.First(), out lastCalculatedVersion)) return false;
-
-    //    fileContent.RemoveAt(0);
-
-    //    try
-    //    {
-    //        var allBytes = File.ReadAllBytes(projectRefAssemblyPath);
-    //        var start = Encoding.UTF8.GetBytes(fileContent.First()).Length;
-    //        var buffer = new byte[allBytes.Length - start];
-
-    //        for (var i = 0; i < allBytes.Length; i++)
-    //        {
-    //            buffer[i] = allBytes[i + start];
-    //        }
-
-    //        refAssembly = SysAssembly.Load(buffer);
-    //        return true;
-
-    //    }
-    //    catch (Exception ex)
-    //    {
-    //        Console.WriteLine(ex);
-    //    }
-
-    //    return false;
-    //}
-
     private bool TryGetLastCalculatedVersionVersion(string gitHash, [MaybeNullWhen(false)] out Version lastCalculatedVersion)
     {
         lastCalculatedVersion = null;
@@ -384,35 +341,6 @@ public class Versioning
 
         return fileContent != null && Version.TryParse(fileContent, out lastCalculatedVersion);
     }
-
-
-
-    private void CompareChanges(IEnumerable<string> gitChanges, IEnumerable<string> referenceVersionList, IEnumerable<string> currentVersionList, 
-        out bool increaseMajor, out bool increaseMinor, out bool increaseBuild, out bool increaseRevision)
-    {
-        increaseMajor = false;
-        increaseBuild = this.IncreaseBuild(gitChanges);
-        increaseRevision = false;
-
-        var currentVersionTempList = currentVersionList.ToList();
-        var referenceVersionTempList = referenceVersionList.ToList();
-
-        foreach (var line in referenceVersionTempList)
-        {
-            if (currentVersionTempList.Remove(line)) continue;
-
-            if (line.StartsWith("referencedAssembly"))
-            {
-                increaseBuild = true;
-                continue;
-            }
-
-            increaseMajor = true;
-        }
-
-        increaseMinor = referenceVersionTempList.Any() && currentVersionTempList.Any(x => !x.StartsWith("referencedAssembly"));
-    }
-
 
     public static Version CalculateVersion(Version version, VersionChange versionChange)
     {
@@ -457,31 +385,6 @@ public class Versioning
         return new Version(versionAsList[0], versionAsList[1], versionAsList[2], versionAsList[3]);
     }
 
-
-
-
-
-    //private IEnumerable<string> GetRefVersionFileContent(string gitHash)
-    //{
-    //    var versioningDir = Path.Combine(this._projectDirName, ".versioning");
-    //    if (!Directory.Exists(versioningDir)) Directory.CreateDirectory(versioningDir);
-
-
-    //    var refVersionInfoFileName = Path.Combine(versioningDir, string.Concat(Path.GetFileName(this._targetFileName), $".{gitHash}.versionInfo"));
-
-    //    if (File.Exists(refVersionInfoFileName)) return File.ReadLines(refVersionInfoFileName);
-
-    //    var defaultRefVersionInfoFileName = Path.Combine(this._projectDirName, projectVersionInfoFileName);
-
-    //    if (!File.Exists(defaultRefVersionInfoFileName)) return Enumerable.Empty<string>();
-    //    File.Copy(defaultRefVersionInfoFileName, refVersionInfoFileName);
-    //    return File.ReadLines(refVersionInfoFileName);
-
-    //}
-
-
-
-
     private bool TryGetRefAssemblyVersion(string gitHash, [MaybeNullWhen(false)] out Version version)
     {
         version = null;
@@ -496,12 +399,6 @@ public class Versioning
         version = SysAssembly.Load(File.ReadAllBytes(refAssemblyPath)).GetName().Version;
         return version != null;
     }
-
-
-  
-
-
-
 
     private  bool TryGetRefAssemblyFileInfo(string gitHash, [MaybeNullWhen(false)]out FileInfo fileInfo)
     {
@@ -533,9 +430,6 @@ public class Versioning
         return true;
     }
 
-
-
-
     private void RememberLastCalculatedVersion(string gitHash, Version calculatedVersion)
     {
         var versioningDir = Path.Combine(this._projectDirName, ".versioning", gitHash);
@@ -545,53 +439,12 @@ public class Versioning
             string.Concat(Path.GetFileName(this._targetFileName), versionInfoFileName)), calculatedVersion.ToString());
     }
 
-
     private void CopyTargetFileToProjectRefFile()
     {
         if (!File.Exists(this._targetFileName)) return;
 
         var projectRefAssemblyPath = Path.Combine(this._projectDirName, versionInfoFileName);
         File.Copy(this._targetFileName, projectRefAssemblyPath, true);
-    }
-
-
-
-
-
-
-
-
-
-    //private void CreateRefVersionFileIfNotExist(string gitHash, IEnumerable<string> fileContent, Version currentVersion)
-    //{
-    //    var versioningDir = Path.Combine(this._projectDirName, ".versioning");
-    //    if (!Directory.Exists(versioningDir)) Directory.CreateDirectory(versioningDir);
-
-    //    var refVersionInfoFileName = Path.Combine(versioningDir, string.Concat(Path.GetFileName(this._targetFileName), $".{gitHash}.versionInfo"));
-
-    //    if (!File.Exists(refVersionInfoFileName))
-    //    {
-    //        var fileContentAsList = fileContent.ToList();
-    //        fileContentAsList.Insert(0, currentVersion.ToString());
-    //        File.WriteAllLines(refVersionInfoFileName, fileContentAsList);
-    //    }
-    //}
-
-    private static bool TryGetRefVersion(IEnumerable<string> refVersionFileContent, [MaybeNullWhen(false)] out Version version)
-    {
-        version = null;
-        var firstLine = refVersionFileContent.FirstOrDefault();
-        return firstLine != null && Version.TryParse(firstLine, out version);
-    }
-
-    private static void RemoveRefVersion(IList<string> refVersionFileContent)
-    {
-        var firstLine = refVersionFileContent.FirstOrDefault();
-
-        if (firstLine != null && Version.TryParse(firstLine, out _))
-        {
-            refVersionFileContent.RemoveAt(0);
-        }
     }
 
     private bool IncreaseBuild(IEnumerable<string> gitChanges)
@@ -621,25 +474,6 @@ public class Versioning
         return version;
     }
 
-    private void WriteProjectVersionInfoFile(IEnumerable<string> fileContent, Version currentVersion)
-    {
-        var defaultRefVersionInfoFileName = Path.Combine(this._projectDirName, versionInfoFileName);
-
-        var fileContentAsList = fileContent.ToList();
-
-        fileContentAsList.Insert(0, currentVersion.ToString());
-        File.WriteAllLines(defaultRefVersionInfoFileName, fileContentAsList);
-    }
-
-    //private bool TryGetProjectRefVersion([MaybeNullWhen(false)] out Version version)
-    //{
-    //    version = null;
-    //    var defaultRefVersionInfoFileName = Path.Combine(this._projectDirName, projectVersionInfoFileName);
-
-    //    return File.Exists(defaultRefVersionInfoFileName) && 
-    //           TryGetRefVersion(File.ReadLines(defaultRefVersionInfoFileName), out version);
-    //}
-
     private static bool TryFindGitRepositoryDirName(string? startDirectory, out string gitRepositoryDirName)
     {
         gitRepositoryDirName = string.Empty;
@@ -659,160 +493,6 @@ public class Versioning
         }
 
         return false;
-    }
-
-    private static System.Reflection.Assembly CreateAssembly(FileInfo assemblyFileInfo)
-    {
-        var assemblyFile = assemblyFileInfo.FullName;
-
-        AppDomain.CurrentDomain.AssemblyResolve += (_, args) =>
-        {
-            var fileName = args.Name.Split(", ", StringSplitOptions.RemoveEmptyEntries).First();
-            var directoryName = assemblyFileInfo.DirectoryName;
-
-            if (directoryName == null) return null;
-
-            fileName = Directory.GetFiles(directoryName, $"{fileName}.*").FirstOrDefault();
-
-            return fileName == null ? null : System.Reflection.Assembly.Load(File.ReadAllBytes(fileName));
-        };
-
-        return System.Reflection.Assembly.LoadFile(assemblyFile);
-    }
-
-    private static IEnumerable<string> CreateRefInfos(System.Reflection.Assembly assembly)
-    {
-        //yield return GetAssemblyVersion(assembly).ToString();
-
-
-            var v = assembly.GetName().Version;
-
-
-        foreach (var result in assembly.Modules.Select(CreateRefInfo))
-        {
-            yield return result;
-        }
-
-        foreach (var exportedType in assembly.ExportedTypes)
-        {
-            foreach (var result in CreateRefInfo(exportedType))
-            {
-                yield return result;
-            }
-        }
-
-        foreach (var assemblyName in assembly.GetReferencedAssemblies())
-        {
-            yield return $"referencedAssembly:{assemblyName.FullName}";
-        }
-
-
-        //foreach (var exportedType in assembly.GetCustomAttributesData())
-        //{
-        //    foreach (var result in CreateRefInfo(exportedType))
-        //    {
-        //        yield return result;
-        //    }
-        //}
-
-
-
-
-
-
-
-        //result.Add($"{major}.{minor}.{build}.{revision}");
-        //result.AddRange(assembly.Modules.Select(CreateModuleRefInfo));
-
-        //foreach (var exportedType in assembly.ExportedTypes)
-        //{
-        //    result.AddRange(CreateTypeRefInfo(exportedType));
-        //}
-
-        //return result;
-    }
-
-    private static string CreateRefInfo(Module module)
-    {
-        return $"module:{module.Name}";
-    }
-
-    internal static IEnumerable<string> CreateRefInfo(Type type)
-    {
-        if (!type.IsPublic) return Enumerable.Empty<string>();
-
-        var result = new List<string> { $"type:{type.FullName}:{type.BaseType?.FullName}:{type.IsAbstract}:{type.IsInterface}:{type.IsEnum}" };
-
-        result.Sort();
-
-        if (type.IsEnum)
-        {
-            var hasFlag = type.GetCustomAttributes(true).OfType<FlagsAttribute>().Any();
-
-            var enumNames = Enum.GetNames(type);
-            var enumValues = GetEnumValues(type).ToArray();
-
-            result.AddRange(enumNames.Select((t, i) => $"enum:{type.FullName}:{hasFlag}:{t}:{enumValues[i]}"));
-
-            return result;
-        }
-        else
-        {
-            result.AddRange(type.GetMethods(BindingFlags.Instance | BindingFlags.Public).Select(CreateRefInfo));
-            result.AddRange(type.GetMethods(BindingFlags.Static | BindingFlags.Public).Select(CreateRefInfo));
-            result.AddRange(type.GetInterfaces().Select(@interface => $"type:{type.FullName}:interface:{@interface.FullName}, {@interface.Assembly.GetName().Name}"));
-        }
-
-        return type.IsInterface || type.IsEnum ? new[] { string.Join('|', result) } : result.Where(x => !string.IsNullOrEmpty(x));
-    }
-
-    private static string CreateRefInfo(MethodInfo methodInfo)
-    {
-        if (methodInfo.DeclaringType == typeof(object)) return string.Empty;
-        var parameters = string.Join(':', methodInfo.GetParameters().Select(CreateRefInfo));
-        var genericArguments = string.Join(':', methodInfo.GetGenericArguments().Select(CreateRefInfo));
-
-        return $"methodInfo:{methodInfo.DeclaringType}.{methodInfo.Name}:{methodInfo.ReturnType.FullName}:{methodInfo.IsAbstract}:{methodInfo.IsStatic}:{parameters}:{genericArguments}";
-    }
-
-    private static string CreateRefInfo(ParameterInfo parameterInfo)
-    {
-        return $"{{parameterInfo:{parameterInfo.ParameterType}:{parameterInfo.Position}:{parameterInfo.IsOut}:{parameterInfo.IsIn}:{parameterInfo.IsOptional}:{parameterInfo.HasDefaultValue}:{parameterInfo.DefaultValue}}}";
-    }
-
-    private static string CreateRefInfo(Attribute attribute)
-    {
-        return $"{{attribute:{attribute.IsDefaultAttribute()}}}";
-    }
-
-    private static string CreateRefInfo(CustomAttributeData customAttributeData)
-    {
-        return $"{{customAttributeData:{customAttributeData.AttributeType}:{customAttributeData.Constructor}}}";
-    }
-
-    private static string CreateRefInfo(ConstructorInfo constructorInfo)
-    {
-        if (constructorInfo.IsPrivate) return string.Empty;
-
-        return $"{{constructorInfo:{constructorInfo}:{constructorInfo.IsStatic}}}";
-    }
-
-    private static string CreateRefInfo(MemberInfo memberInfo)
-    {
-        return $"{{memberInfo:{memberInfo}}}";
-    }
-
-    private static IEnumerable<object> GetEnumValues(Type enumType)
-    {
-        var enumUnderlyingType = Enum.GetUnderlyingType(enumType);
-        var enumValues = Enum.GetValues(enumType);
-
-        for (var i = 0; i < enumValues.Length; i++)
-        {
-            var value = enumValues.GetValue(i);
-            if (value == null) continue;
-            yield return Convert.ChangeType(value, enumUnderlyingType);
-        }
     }
 
     private static void UpdateProjectFile(string projectFileName, Version assemblyVersion, string versionSuffix,  string sourceRevisionId)
