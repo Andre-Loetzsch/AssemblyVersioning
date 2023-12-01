7d685bdcc987d79d2bb7cf25935f199baf33e2f9
Andre-Loetzsch <andre.loetzsch@live.de>
2023-12-01 13:52:23 +0100

    dotnet tool and msbuild task created

---
+++ tests/ConsoleApp80/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp80
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public void Test1()
+        {
+
+        }
+    }
+
+   
+}
