ad6ae603c04b3dc0238c87895dfc1182f631ea82
Andre-Loetzsch <andre.loetzsch@live.de>
2023-12-07 16:00:49 +0100

    save status

---
+++ tests/Oleander.Assembly.Versioning.Tests/NuGetTests.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace Oleander.Assembly.Versioning.Tests;
+
+public class NuGetTests
+{
+    [Fact]
+    public async void TestDownload()
+    {
+        await NuGetHelper.DownloadPackageAsync("Newtonsoft.Json");
+        await NuGetHelper.DownloadPackageAsync("Oleander.Extensions.Logging.Abstractions");
+
+    }
+}
