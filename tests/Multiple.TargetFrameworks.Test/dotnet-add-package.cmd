78d28d8470e236e8211d3f8e19867b03ea6a3721
André Lötzsch <andre.loetzsch@live.de>
2023-12-22 21:03:33 +0100

    save status

--- tests/Multiple.TargetFrameworks.Test/dotnet-add-package.cmd
+++ tests/Multiple.TargetFrameworks.Test/dotnet-add-package.cmd
@@ -1,5 +1,5 @@
 rem dotnet remove package Oleander.Assembly.Versioning.BuildTask
-dotnet add package Oleander.Assembly.Versioning.BuildTask -s %~dp0\..\..\src\Oleander.Assembly.Versioning.BuildTask\bin\Debug
+dotnet add package Oleander.Assembly.Versioning.BuildTask -s %~dp0\..\..\src\Oleander.Assembly.Versioning.BuildTask\bin\Debug --prerelease
 rem dotnet restore -f
 pause
 
