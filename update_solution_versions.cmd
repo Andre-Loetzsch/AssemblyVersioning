e4c677ed739f292a28f654c4bd7f9549ad82d956
André Lötzsch <86790735+Andre-Loetzsch@users.noreply.github.com>
2024-01-17 17:29:54 +0100

    Warning CS8600 fixed (#67)
    * Warning CS8600 fixed

--- update_solution_versions.cmd
+++ update_solution_versions.cmd
@@ -1,16 +1,8 @@
 "%~dp0\src\Oleander.Assembly.Versioning.Tool\bin\Debug\net8.0\Oleander.Assembly.Versioning.Tool.exe" update -t "%~dp0\src\Oleander.Assembly.Comparers\bin\Debug\netstandard2.0\Oleander.Assembly.Comparers.dll" 
-pause
 "%~dp0\src\Oleander.Assembly.Versioning.Tool\bin\Debug\net8.0\Oleander.Assembly.Versioning.Tool.exe" update -t "%~dp0\src\Oleander.Assembly.Comparers\bin\Debug\net8.0\Oleander.Assembly.Comparers.dll" 
-pause
 "%~dp0\src\Oleander.Assembly.Versioning.Tool\bin\Debug\net8.0\Oleander.Assembly.Versioning.Tool.exe" update -t "%~dp0\src\Oleander.Assembly.Versioning\bin\Debug\net8.0\Oleander.Assembly.Versioning.dll" 
-pause
 "%~dp0\src\Oleander.Assembly.Versioning.Tool\bin\Debug\net8.0\Oleander.Assembly.Versioning.Tool.exe" update -t "%~dp0\src\Oleander.Assembly.Versioning\bin\Debug\netstandard2.0\Oleander.Assembly.Versioning.dll" 
-pause
 "%~dp0\src\Oleander.Assembly.Versioning.Tool\bin\Debug\net8.0\Oleander.Assembly.Versioning.Tool.exe" update -t "%~dp0\src\Oleander.Assembly.Versioning.BuildTask\bin\Debug\netstandard2.0\Oleander.Assembly.Versioning.BuildTask.dll" 
-pause
 "%~dp0\src\Oleander.Assembly.Versioning.Tool\bin\Debug\net8.0\Oleander.Assembly.Versioning.Tool.exe" update -t "%~dp0\src\Oleander.Assembly.Versioning.BuildTask\bin\Debug\net8.0\Oleander.Assembly.Versioning.BuildTask.dll" 
-pause
 "%~dp0\src\Oleander.Assembly.Versioning.Tool\bin\Debug\net8.0\Oleander.Assembly.Versioning.Tool.exe" update -t "%~dp0\src\Oleander.Assembly.Versioning.Tool\bin\Debug\net8.0\Oleander.Assembly.Versioning.Tool.dll" 
-pause
 "%~dp0\src\Oleander.Assembly.Versioning.Tool\bin\Debug\net8.0\Oleander.Assembly.Versioning.Tool.exe" update -t "%~dp0\tests\Oleander.Assembly.Versioning.Tests\bin\Debug\net8.0\Oleander.Assembly.Versioning.Tests.dll"
-pause
